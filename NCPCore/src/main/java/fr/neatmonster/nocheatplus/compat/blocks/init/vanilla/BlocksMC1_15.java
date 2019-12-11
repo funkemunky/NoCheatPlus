bcbb1d6878d833788db8707538ebf71501f7d89e
MarkElf <13339283+MarkElf@users.noreply.github.com>
2019-12-10 23:35:38 -0500

    Update BlocksMC1_15.java
    Fixed compile error.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/compat/blocks/init/vanilla/BlocksMC1_15.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/compat/blocks/init/vanilla/BlocksMC1_15.java
@@ -7,7 +7,6 @@ import fr.neatmonster.nocheatplus.compat.blocks.BlockPropertiesSetup;
 import fr.neatmonster.nocheatplus.compat.blocks.init.BlockInit;
 import fr.neatmonster.nocheatplus.config.WorldConfigProvider;
 import fr.neatmonster.nocheatplus.logging.StaticLog;
-//import fr.neatmonster.nocheatplus.utilities.map.BlockFlags;
 import fr.neatmonster.nocheatplus.utilities.map.BlockProperties;
 
 public class BlocksMC1_15 implements BlockPropertiesSetup{
@@ -22,8 +21,8 @@ public class BlocksMC1_15 implements BlockPropertiesSetup{
 	public void setupBlockProperties(WorldConfigProvider<?> worldConfigProvider) {
 		BlockProperties.setBlockProps("BEE_NEST", new BlockProperties.BlockProps(BlockProperties.woodAxe, 0.3f, BlockProperties.secToMs(0.45, 0.25, 0.15, 0.1, 0.1, 0.05)));
 		BlockProperties.setBlockProps("BEEHIVE", new BlockProperties.BlockProps(BlockProperties.woodAxe, 0.6f, BlockProperties.secToMs(0.9, 0.45, 0.25, 0.15, 0.15, 0.1)));
-		BlockFlags.setFlagsAs("HONEY_BLOCK", BridgeMaterial.STONE_BRICK_STAIRS);
-		BlockProperties.setBlockProps("HONEYCOMB_BLOCK" new BlockProperties.BlockProps(BlockProperties.woodAxe, 0.6f, BlockProperties.secToMs(0.9, 0.9, 0.9, 0.9, 0.9, 0.9)));
+                BlockInit.setAs("HONEY_BLOCK", Material.TNT);
+		BlockProperties.setBlockProps("HONEYCOMB_BLOCK", new BlockProperties.BlockProps(BlockProperties.woodAxe, 0.6f, BlockProperties.secToMs(0.9, 0.9, 0.9, 0.9, 0.9, 0.9)));
 		StaticLog.logInfo("Added block-info for Minecraft 1.15 blocks.");
 	}
 
